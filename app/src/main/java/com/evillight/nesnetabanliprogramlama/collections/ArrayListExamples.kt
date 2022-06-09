aceb05a203cf62cc2c5b4b5c40aed3ab6be649dc
Evilllight <hfv856@gmail.com>
2022-06-09 17:29:17 +0300

    a

--- app/src/main/java/com/evillight/nesnetabanliprogramlama/collections/ArrayListExamples.kt
+++ app/src/main/java/com/evillight/nesnetabanliprogramlama/collections/ArrayListExamples.kt
@@ -1,5 +1,28 @@
 package com.evillight.nesnetabanliprogramlama.collections
 
 fun main(){
+    val meyveler=ArrayList<String>()
 
+    meyveler.add("Çilek")
+    meyveler.add("Muz")
+    meyveler.add("Elma")
+    meyveler.add("Kivi")
+    meyveler.add("Kiraz")
+
+    println(meyveler)
+
+    val str=meyveler.get(2)
+    println(str)
+
+    val str1=meyveler[4]
+    println(str1)
+
+    meyveler.add("Mandalina")
+    println(meyveler)
+
+    meyveler[2]="Ananas"
+    println(meyveler)
+
+    meyveler.add(3,"Portakal")
+    println(meyveler)
 }
