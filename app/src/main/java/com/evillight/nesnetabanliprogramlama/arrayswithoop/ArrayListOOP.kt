d6443866e88ef58056d34af11de4552fba70a2d5
Evilllight <hfv856@gmail.com>
2022-06-10 19:17:55 +0300

    a

--- app/src/main/java/com/evillight/nesnetabanliprogramlama/arrayswithoop/ArrayListOOP.kt
+++ app/src/main/java/com/evillight/nesnetabanliprogramlama/arrayswithoop/ArrayListOOP.kt
@@ -1,2 +1,18 @@
 package com.evillight.nesnetabanliprogramlama.arrayswithoop
 
+fun main(){
+    val u1=Urun(1,"Saat",150.0)
+    val u2=Urun(2,"Televizyon",1750.0)
+    val u3=Urun(3,"Bilgisayar",960.0)
+
+    val urunler=ArrayList<Urun>()
+    urunler.add(u1)
+    urunler.add(u2)
+    urunler.add(u3)
+
+    for (urun in urunler){
+        println("Ürün no :${urun.urunNo}")
+        println("Ürün adı :${urun.urunAd}")
+        println("Ürün fiyat :${urun.urunFiyat }")
+    }
+}
