28797f71e4ef9793a468804dc755977ed86ea41c
Evilllight <hfv856@gmail.com>
2022-06-10 19:09:49 +0300

    a

---
+++ app/src/main/java/com/evillight/nesnetabanliprogramlama/arrayswithoop/Urun.kt
@@ -0,0 +1,4 @@
+package com.evillight.nesnetabanliprogramlama.arrayswithoop
+
+class Urun {
+}
--- app/src/main/java/com/evillight/nesnetabanliprogramlama/collections/ArrayListDonguler.kt
+++ app/src/main/java/com/evillight/nesnetabanliprogramlama/collections/ArrayListDonguler.kt
@@ -2,4 +2,17 @@ package com.evillight.nesnetabanliprogramlama.collections
 
 fun main(){
     val meyveler=ArrayList<String>()
+
+    meyveler.add("Çilek")
+    meyveler.add("Muz")
+    meyveler.add("Elma")
+    meyveler.add("Kivi")
+    meyveler.add("Kiraz")
+
+    for(meyve in meyveler){
+        println(meyve)
+    }
+    for ((indeks,meyve) in meyveler.withIndex()){
+        println("$indeks,$meyve")
+    }
 }
