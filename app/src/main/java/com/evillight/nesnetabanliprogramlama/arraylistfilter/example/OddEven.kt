722d0f737b6a27b9c5734d312c1d8229075c6e25
Evilllight <hfv856@gmail.com>
2022-06-13 00:08:30 +0300

    main

--- app/src/main/java/com/evillight/nesnetabanliprogramlama/arraylistfilter/example/OddEven.kt
+++ app/src/main/java/com/evillight/nesnetabanliprogramlama/arraylistfilter/example/OddEven.kt
@@ -1,5 +1,34 @@
 package com.evillight.nesnetabanliprogramlama.arraylistfilter.example
 
 fun main(){
+    val sayilar=ArrayList<Int>()
+    sayilar.add(50)
+    sayilar.add(11)
+    sayilar.add(23)
+    sayilar.add(74)
+    sayilar.add(87)
+    sayilar.add(94)
 
+    val odd=ArrayList<Int>()
+    val even=ArrayList<Int>()
+
+    for (s in sayilar){
+        val sonuc=s%2
+
+        if (sonuc==0){
+            even.add(s)
+        }else{
+            odd.add(s)
+        }
+
+    }
+    //println("$odd - $even")
+    println("ODD")
+    for (t in odd){
+        println(t)
+    }
+    println("EVEN")
+    for (t in even){
+        println(t)
+    }
 }
